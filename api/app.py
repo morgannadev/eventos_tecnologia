a5fe9ee330a7a0120b64daa5be3d08fb5d0b9206
morgannadev <morgcontato@gmail.com>
2023-12-16 20:30:33 -0300

    ajuste do ambiente de teste

--- api/app.py
+++ api/app.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
-import uvicorn
+
+# import uvicorn
 
 app = FastAPI()
+
 
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
-
-def start():
-    uvicorn.run("api.main:app", host = "127.0.0.1")
